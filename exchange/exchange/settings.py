35c3a2497a60217e2b9cdbf904eaaaa870c8edf5
Noumedem Tami Florian <noumedemtamiflorian@gmail.com>
2021-06-13 21:44:42 +0100

    Creation de la base de donnee

--- exchange/exchange/settings.py
+++ exchange/exchange/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'exchange.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': "exchange",
+        'USER': 'root',
+        'PASSWORD': 'root'
     }
 }
 
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr-FR'
 
 TIME_ZONE = 'UTC'
 
