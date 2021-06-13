248b8238a5dec63064573c0ced8c0de712099c33
Noumedem Tami Florian <noumedemtamiflorian@gmail.com>
2021-06-13 21:57:26 +0100

    Creation de la table contact

--- exchange/contacts/models.py
+++ exchange/contacts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=200, unique=True)
+    number = models.IntegerField()
+
+    def __str__(self):
+        return self.name
